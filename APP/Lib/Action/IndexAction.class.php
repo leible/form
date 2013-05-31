6520a33d625515ce15c40d85654946ecfab3b7ad
123 <123@long>
2013-05-31 15:54:13 +0800

    添加了add模块，edit模块，add方法，index方法，delete方法

--- APP/Lib/Action/IndexAction.class.php
+++ APP/Lib/Action/IndexAction.class.php
@@ -5,7 +5,12 @@ class IndexAction extends Action {
 
     //查询数据
     public function index() {
-
+        $user=M("user");
+        $userList=$user->select();
+        $this->display();
+    }
+    //添加信息界面
+        public function add() {
         $this->display();
     }
     //添加信息
@@ -24,5 +29,47 @@ class IndexAction extends Action {
 
 
     }
+        //修改信息界面
+        public function edit($id) {
+            if(!empty($id)){
+                $user=M("user");
+                $vo=$user->getById($id);
+                if($vo){
 
+                }
+
+            }
+            $this->display();
+        }
+
+       //修改信息
+        public function update() {
+        $user=M("user");
+        if($vo=$user->create()){
+            $list=$user->save();
+            if($AddList!==false){
+                $this->success('数据更新成功');
+            }else{
+                $this->error('数据更新失败');
+            }
+        }else{
+            $this->error($user->getError());
+        }
+
+    }
+
+    //删除信息
+    public function delete($id){
+        if(!empty($id)){
+            $user=M("user");
+            $result=$user->delete($id);
+            if($result!==false){
+                $this->success("删除成功");
+            }else{
+                $this->error("删除失败");
+            }
+        }else{
+            $this->error("删除信息失败");
+        }
+    }
 }
