73e3e2588e463dda1e6f5fe78d6830b393ea1ff3
123 <123@long>
2013-05-31 17:16:02 +0800

    add

--- APP/Lib/Action/IndexAction.class.php
+++ APP/Lib/Action/IndexAction.class.php
@@ -5,21 +5,23 @@ class IndexAction extends Action {
 
     //查询数据
     public function index() {
-        $user=M("user");
-        $userList=$user->select();
-        $this->assign("userList",$userlist);
+        $user = M("user");
+        $userList = $user->select();
+        $this->assign("userList", $userList);
         $this->display();
     }
+
     //添加信息界面
-        public function add() {
+    public function add() {
         $this->display();
     }
+
     //添加信息
-    public function insert(){
+    public function insert() {
         $username = $_POST['username'];
         $sex = $_POST['sex'];
         $area = $_POST['area'];
-        $hobby= serialize($_POST["hobby"]);
+        $hobby = serialize($_POST["hobby"]);
         $message = $_POST['message'];
 
         $data = array(
@@ -29,61 +31,59 @@ class IndexAction extends Action {
             'hobby' => $hobby,
             'message' => $message
         );
-        $user=M("user");
-        if($data){
-            $AddList=$user->add($data);
-            if($AddList!==false){
+        $user = M("user");
+        if ($data) {
+            $AddList = $user->add($data);
+            if ($AddList !== false) {
                 $this->success('数据插入成功');
-            }else{
+            } else {
                 $this->error('数据插入失败');
             }
-        }else{
+        } else {
             $this->error($user->getError());
         }
-
-
     }
-        //修改信息界面
-        public function edit($id) {
-            if(!empty($id)){
-                $user=M("user");
-                $vo=$user->getById($id);
-                if($vo){
 
-                }
+    //修改信息界面
+    public function edit($id) {
+        if (!empty($id)) {
+            $user = M("user");
+            $vo = $user->getById($id);
+            if ($vo) {
 
             }
-            $this->display();
         }
+        $this->display();
+    }
 
-       //修改信息
-        public function update() {
-        $user=M("user");
-        if($vo=$user->create()){
-            $list=$user->save();
-            if($AddList!==false){
+    //修改信息
+    public function update() {
+        $user = M("user");
+        if ($vo = $user->create()) {
+            $list = $user->save();
+            if ($AddList !== false) {
                 $this->success('数据更新成功');
-            }else{
+            } else {
                 $this->error('数据更新失败');
             }
-        }else{
+        } else {
             $this->error($user->getError());
         }
-
     }
 
     //删除信息
-    public function delete($id){
-        if(!empty($id)){
-            $user=M("user");
-            $result=$user->delete($id);
-            if($result!==false){
+    public function delete($id) {
+        if (!empty($id)) {
+            $user = M("user");
+            $result = $user->delete($id);
+            if ($result !== false) {
                 $this->success("删除成功");
-            }else{
+            } else {
                 $this->error("删除失败");
             }
-        }else{
+        } else {
             $this->error("删除信息失败");
         }
     }
+
 }
